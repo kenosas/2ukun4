a3e4d55a76827026bc86f655c65a7a1a8ccc7f92
kenosas <111889266+kenosas@users.noreply.github.com>
2024-07-01 11:03:48 -0300

    Add files via upload

---
+++ victor-IA/main.js
@@ -0,0 +1,20 @@
+const caixaPrincipal = document.querySelector(".caixa-Principal");
+const caixaPerguntas = document.querySelector(".caixa-Perguntas");
+const caixaAlternativas = document.querySelector(".caixa-Alternativas");
+const caixaResultados = document.querySelector(".caixa-Resltados");
+const TextoResultado = document.querySelector(".Texto-Resultado");
+
+const caixaresultado1 = document.querySelector(".caixa-principal");
+
+const Perguntas = [
+
+   {
+     enunciado: "qual o esporte mais conhecido no mundo?   ";
+     alternativas [
+        "Sim"
+        "O esporte mais conhecido no mundo é o futebol"
+     ]
+   }
+
+]
+
